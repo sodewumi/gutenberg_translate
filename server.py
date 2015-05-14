9beb81f92deea61550072df6a7e18229c684a086
Hackbright Student <info@hackbrightacademy.com>
2015-05-14 14:22:38 -0700

    Connected display chapters to database

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 from flask import flash, Flask, redirect, render_template, request, session, jsonify
 import jinja2
 from model import Book, User, Genre, Chapter, Paragraph, Translation, connect_to_db, db
-from gutenberg.acquire import load_etext
-from gutenberg.cleanup import strip_headers
 
 app = Flask(__name__)
 app.secret_key = 'will hook to .gitignore soon'
@@ -35,17 +33,16 @@ def display_translation_page():
         Displays the a chapter of the book. 
         When page first loads, chapter starts at 0.
     """
+    number_of_chapters = db.session.query(Chapter).count()
 
-    book_chapters = open_file()
-    number_of_chapters = len(book_chapters)
     
     chapter_chosen = request.args.get("chapter_selection")
 
     # checks if the form has been submitted
     if chapter_chosen:
-        display_chapter = book_chapters[int(chapter_chosen)]
+        display_chapter = db.session.query(Paragraph).filter_by(chapter_id = chapter_chosen)
     else:
-        display_chapter = book_chapters[0]
+        display_chapter = db.session.query(Paragraph).filter_by(chapter_id = 0)
 
     return render_template("translation_page.html",
         number_of_chapters = number_of_chapters, 
@@ -74,14 +71,11 @@ def open_file():
     tail_deliminator_idx = book_string.index("*** END OF THIS PROJECT GUTENBERG")
 
     book_string = book_string[head_deliminator_idx + len(head_deliminator) : tail_deliminator_idx]
-    book = {}
+
     book_chapters = book_string.split("Chapter")
 
     for i in range(len(book_chapters)):
         book_chapters[i] = book_chapters[i].split('\n\n')
-
-    for c, chapters in enumerate(book_chapters):
-        book.setdefault(c, chapters)
 
     return book_chapters
 
@@ -89,18 +83,19 @@ def book_database():
     """ Pushs newly created books into a database"""
 
     book = open_file()
-    # haven't reseeded database to make sure chapters start at 1 and not 0)
-    for c, chapters in enumerate(book, 1):
+
+    # I start at 0, because I want to copyright information to show
+    for c, chapters in enumerate(book, 0):
+        # change book_id in the future
         db.session.add(Chapter(chapter_number=c, book_id=1))
         for paragrpahs in chapters:
-            print paragrpahs.__repr__
-            # if paragrpahs.strip() != "":
-            #     db.session.add(Paragraph(untranslated_paragraph=paragrpahs))
+            db.session.add(Paragraph(untranslated_paragraph=paragrpahs, chapter_id=c))
 
     db.session.commit()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
     connect_to_db(app)
     # book_database()
+    app.run(debug=True)
+
